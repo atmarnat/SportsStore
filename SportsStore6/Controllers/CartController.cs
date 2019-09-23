f342aad054fe0a9c21a5c16915cb0665e7125b9f
atmar <nathaniel.atmar@outlook.com>
2019-09-23 13:34:40 -0700

    Completed through page 274

--- SportsStore6/Controllers/CartController.cs
+++ SportsStore6/Controllers/CartController.cs
@@ -6,16 +6,19 @@ using SportsStore6.Models;
 using SportsStore6.Models.ViewModels;
 
 namespace SportsStore6.Controllers{
-    public class CartController : Controller {
+    public class CartController : Controller
+    {
         private IProductRepository repository;
+        private Cart cart;
 
-        public CartController(IProductRepository repo) {
+        public CartController(IProductRepository repo, Cart cartService) {
             repository = repo;
+            cart = cartService;
         }
 
         public ViewResult Index(string returnUrl) {
             return View(new CartIndexViewModel {
-                Cart = GetCart(),
+                Cart = cart,
                 ReturnUrl = returnUrl
             });
         }
@@ -23,34 +26,21 @@ namespace SportsStore6.Controllers{
         public RedirectToActionResult AddToCart(int productId, string returnUrl) {
             Product product = repository.Products
                 .FirstOrDefault(p => p.ProductID == productId);
-
             if (product != null) {
-                Cart cart = GetCart();
                 cart.AddItem(product, 1);
-                SaveCart(cart);
             }
             return RedirectToAction("Index", new { returnUrl });
         }
+
         public RedirectToActionResult RemoveFromCart(int productId,
             string returnUrl) {
-                Product product = repository.Products
+            Product product = repository.Products
                 .FirstOrDefault(p => p.ProductID == productId);
 
             if (product != null) {
-                Cart cart = GetCart();
                 cart.RemoveLine(product);
-                SaveCart(cart);
             }
             return RedirectToAction("Index", new { returnUrl });
-        }
-
-        private Cart GetCart() {
-            Cart cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
-            return cart;
-        }
-
-        private void SaveCart(Cart cart) {
-            HttpContext.Session.SetJson("Cart", cart);
         }
     }
 }
--- SportsStore6/Startup.cs
+++ SportsStore6/Startup.cs
@@ -27,6 +27,8 @@ namespace SportsStore6
                 options.UseSqlServer(
                     Configuration["Data:SSDB:ConnectionString"]));
             services.AddTransient<IProductRepository, EFProductRepository>();
+            services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddMvc();
             services.AddMemoryCache();
             services.AddSession();
