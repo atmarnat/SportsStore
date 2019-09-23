65c91e6130eb43206af7befdf6499f6cd3294d45
atmar <nathaniel.atmar@outlook.com>
2019-09-23 14:19:25 -0700

    Completed through page 283, checkout page done

---
+++ SportsStore6/Controllers/OrderController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SportsStore6.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace SportsStore6.Controllers
+{
+    public class OrderController : Controller
+    {
+        public ViewResult Checkout() => View(new Order());
+    }
+}
