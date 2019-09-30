0951cf1e626c3458e5e483e318910e98072e0235
atmar <nathaniel.atmar@outlook.com>
2019-09-30 08:55:13 -0700

    Went back to local DB, fixed everything on remigration

--- SportsStore6/Migrations/20190930154349_ShippedOrders.Designer.cs
+++ SportsStore6/Migrations/20190930154349_ShippedOrders.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SportsStore6.Models;
 
 namespace SportsStore6.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190930154349_ShippedOrders")]
+    partial class ShippedOrders
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
